6cbba8a83e6558c40617a3a1a9b0b80872d08b44
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-07-21 13:03:52 +0300

    fix: Define name and hometown as properties instead of attributes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,6 +3,24 @@ class Band:
         self.name = name
         self.hometown = hometown
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+    
+    @property
+    def hometown(self):
+        return self._hometown
+
+    @hometown.setter
+    def hometown(self, hometown):
+        if isinstance(hometown, str) and len(hometown) > 0 and not(hasattr(self, '_hometown')):
+            self._hometown = hometown
+
     def concerts(self):
         pass
 
